3cdbe8317f8c9c1b8ab3c99a939f45b08b992dcf
Nikolaj Sidorenco <Nikolaj176@hotmail.com>
2017-03-10 19:50:33 +0100

    local cleanup

--- lib/csgo_events/crawler.ex
+++ lib/csgo_events/crawler.ex
@@ -13,8 +13,20 @@ defmodule CsgoEvents.Crawler do
 
   #Server API
   def init(_args) do
-    {:ok, fetch_data!()}
+    Process.send_after(self(), :started, 0)
+    {:ok, []}
   end
+
+  def handle_info(:started, _) do
+    Process.send_after(self(), :update, 28800000)
+    {:noreply, fetch_data!()}
+  end
+
+  def handle_info(:update, _state) do
+    Process.send_after(self(), :update, 28800000)
+    {:noreply, fetch_data!()}
+  end
+
 
 
   def handle_call(:fetch, _from, state) do

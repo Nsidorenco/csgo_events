14d60a3fa87f8c1bd3dbb64d1072f72828d3621e
Nikolaj Sidorenco <Nikolaj176@hotmail.com>
2017-03-23 02:17:57 +0100

    add Elm

--- apps/api_web/assets/js/app.js
+++ apps/api_web/assets/js/app.js
@@ -19,3 +19,10 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+// Elm hook
+import Elm from '../../priv/static/js/main.js';
+const elmDiv = document.querySelector('#elm-target');
+if (elmDiv) {
+  Elm.Main.embed(elmDiv);
+}
--- apps/api_web/lib/api_web/views/event_view.ex
+++ apps/api_web/lib/api_web/views/event_view.ex
@@ -5,11 +5,7 @@ defmodule Api.Web.EventView do
   def render("index.json", %{events: events}) do
     %{data: render_many(events, EventView, "event.json")}
   end
-
-  def render("show.json", %{event: event}) do
-    %{data: render_one(event, EventView, "event.json")}
-  end
-
+  
   def render("event.json", %{event: event}) do
     %{event_name: event.event_name,
       event_type: event.event_type,
